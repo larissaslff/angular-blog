1d64e8c5d56cae25040151de03e1a10c9dfa0523
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-09-13 21:10:08 -0300

    add router

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,22 @@
+export const dataFake = [
+  {
+    id: "1",
+    photo: 'https://i.em.com.br/bVBRB5kPSwrfmlh_6VZCIP6boAc=/1200x720/smart/imgsapp.em.com.br/app/noticia_127983242361/2023/02/28/1463056/rbd_1_122043.jpg',
+    title: 'Noticia',
+    description: 'description',
+  },
+
+  {
+    id: "2",
+    photo: 'https://i.em.com.br/bVBRB5kPSwrfmlh_6VZCIP6boAc=/1200x720/smart/imgsapp.em.com.br/app/noticia_127983242361/2023/02/28/1463056/rbd_1_122043.jpg',
+    title: 'Noticia',
+    description: 'description',
+  },
+
+  {
+    id: "3",
+    photo: 'https://s2-quem.glbimg.com/ReGkDCYfwXBW8DWc3NlMvcGv1Lo=/0x0:1080x755/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2023/n/X/JUzqPXRTC0ftTnt3h5Rw/unknown-316160418-674958204099004-4106620521380801632-n.jpg',
+    title: 'Noticia',
+    description: 'description',
+  },
+];
